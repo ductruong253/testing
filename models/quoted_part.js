d05aaac55daf15e4d6c8b928e2e4a189fdaa480d
ductruong253 <ddtruong@gmail.com>
2019-07-03 22:38:23 +0700

    Added on 07-03-19 22:38

--- models/quoted_part.js
+++ models/quoted_part.js
@@ -5,15 +5,10 @@ var quotedPartSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "APQP"
   }],
-  quote_num: String,
   quote_item: String,
   customer_part_number: String,
   assigned_cd: String,
   mold: Boolean,
-  pic: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Member"
-  }],
   datasheet: [{
     type: mongoose.Schema.Types.ObjectId,
     ref:"Datasheet"
--- routes/project.js
+++ routes/project.js
@@ -1,27 +1,28 @@
 const express = require('express');
 var router = express.Router()
 var APQP = require('../models/apqp');
+var Qpart = require('../models/quoted_part');
 
 
 
-router.get('/projects',function (req,res) {
-  APQP.find({},function (err,allApqps) {
+router.get('/projects', function(req, res) {
+  APQP.find({}, function(err, allApqps) {
     if (err) {
       console.log(err);
-    }else {
-      res.render('./projects/index',{
+    } else {
+      res.render('./projects/index', {
         apqps: allApqps
       })
     }
   })
 })
 
-router.get('/projects/new',function (req,res) {
+router.get('/projects/new', function(req, res) {
   res.render('projects/new')
 })
 
 //Define new projects
-router.post('/projects',function (req,res) {
+router.post('/projects', function(req, res) {
   console.log('ok');
   var apqp_number = req.body.apqp_number
   var apqp_quote_number = req.body.apqp_quote_number
@@ -29,24 +30,47 @@ router.post('/projects',function (req,res) {
   var apqp_customer_code = req.body.apqp_customer_code
   var apqp_prod_class = req.body.apqp_prod_class
   var apqp_indu_class = req.body.apqp_indu_class
+  var quote_item_arr = req.body.quote_item
 
   var new_APQP = {
     apqp_number: apqp_number,
     quote_num: apqp_quote_number,
-    customer: apqp_customer,
+    customer: apqp_customer.toUpperCase(),
     customer_code: apqp_customer_code,
     product_class: apqp_prod_class,
     industrial_class: apqp_indu_class
   }
-
-  APQP.create(new_APQP,function (err,newlyCreated) {
-    if(err){
+//Create new APQP
+  APQP.create(new_APQP, function(err, newlyCreatedAPQP) {
+    if (err) {
       console.log(err);
-    }else {
-      console.log(newlyCreated);
+    } else {
+      console.log(newlyCreatedAPQP);
       res.redirect("/projects")
     }
   })
+// For each quote_item, create quote item in database
+
+quote_item_arr.forEach(function (quote_item_element) {
+  if (quote_item_element.mold=="True"){
+    var new_Quote_item ={
+      quote_item: quote_item_element.item,
+      customer_part_number: quote_item_element.customer_part_number,
+      assigned_cd: quote_item_element.cd,
+      mold: true
+    }
+  }else {
+    var new_Quote_item ={
+      quote_item: quote_item_element.item,
+      customer_part_number: quote_item_element.customer_part_number,
+      assigned_cd: quote_item_element.cd,
+      mold: false
+    }
+  }
+  console.log(new_Quote_item);
+})
+
+
 })
 
 module.exports = router
